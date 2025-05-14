c96298c35a926594d6ef1a9edc6a0c3e8e9ec450
Savitarx7 <pavlickadrian@gmail.com>
2025-05-14 13:09:04 -0700

    Backup before prebuild

--- metro.config.js
+++ metro.config.js
@@ -1,4 +1,15 @@
-// metro.config.js
-const { getDefaultConfig } = require('@expo/metro-config');
+/** @type {import('metro-config').MetroConfig} */
+module.exports = (async () => {
+  const { getDefaultConfig } = await import('@expo/metro-config');
+  const config = getDefaultConfig(__dirname);
 
-module.exports = getDefaultConfig(__dirname);
+  // Ensure support for .gif and .mp3 files
+  config.resolver.assetExts.push('gif', 'mp3');
+
+  // Optional: some Firebase modules use .cjs extensions
+  if (!config.resolver.sourceExts.includes('cjs')) {
+    config.resolver.sourceExts.push('cjs');
+  }
+
+  return config;
+})();
