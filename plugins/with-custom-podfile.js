75e8c8047d3ae0993e9cd381868113ea0fecbaff
Savitarx7 <pavlickadrian@gmail.com>
2025-05-20 09:41:02 -0700

    Final fix for pod integration

--- plugins/with-custom-podfile.js
+++ plugins/with-custom-podfile.js
@@ -6,19 +6,18 @@ module.exports = function withCustomPodfile(config) {
       ios: {
         podfile(podfileConfig) {
           if (!podfileConfig.contents) {
-            return podfileConfig; // avoid crash if contents is undefined
+            return podfileConfig; // Avoid crash
           }
 
           const lines = podfileConfig.contents.split('\n');
-          const modifiedLines = lines.map(line => {
-            if (line.includes('use_expo_modules!()')) {
-              return `${line}\n  use_frameworks! :linkage => :static\n  use_modular_headers!`;
-            }
-            return line;
-          });
+          const modifiedLines = [];
 
-          if (!modifiedLines.some(line => line.includes('use_modular_headers!'))) {
-            modifiedLines.unshift('use_modular_headers!');
+          for (let line of lines) {
+            if (line.includes('use_expo_modules!()')) {
+              modifiedLines.push('use_modular_headers!');
+              modifiedLines.push('use_frameworks! :linkage => :static');
+            }
+            modifiedLines.push(line);
           }
 
           podfileConfig.contents = modifiedLines.join('\n');
