9c984addec047872cbf655d3dd6c0a076b83daaa
Savitarx7 <pavlickadrian@gmail.com>
2025-08-02 13:54:17 -0400

    chore: remove custom podfile plugins

--- app.config.js
+++ app.config.js
@@ -34,19 +34,8 @@ export default {
       eas: {
         projectId: 'f0fad71c-a7c9-4c22-9356-33a6ca1b0c32',
       },
-      // 👇 Your custom plugin reads the App IDs from here
-      reactNativeGoogleMobileAds: {
-        // Test App IDs from Google. Replace with your real IDs when publishing.
-        ios_app_id: 'ca-app-pub-3940256099942544~1458002511',
-        android_app_id: 'ca-app-pub-3940256099942544~3347511713',
-      },
     },
     plugins: [
-      // ✅ Use your custom plugins
-      './plugins/with-remove-admob-build-phase.js',
-      './plugins/with-admob-app-id.js',
-
-      // Your existing plugins
       [
         'expo-build-properties',
         {
